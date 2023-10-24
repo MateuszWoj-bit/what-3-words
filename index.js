aba57c5eda97052a26fedb2f993e9544a146b95f
Mateusz Wójcikowski <yankes90@gmail.com>
2023-10-25 01:22:22 +0200

    map

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+let map;
+let marker;
+
 function convertToCoordinates() {
-  const apiKey = "YOUR_API_KEY";
+  const apiKey = "YOUR_API_KEY"; 
   const w3wAddress = document.getElementById("w3wInput").value;
+ 
   fetch(
     `https://api.what3words.com/v3/convert-to-coordinates?words=${w3wAddress}`,
     {
@@ -15,6 +19,21 @@ function convertToCoordinates() {
       const coordinates = data.coordinates;
       document.getElementById("coordinates").innerText =
         coordinates.lat + ", " + coordinates.lng;
+
+     
+      if (!map) {
+        map = L.map("map").setView([coordinates.lat, coordinates.lng], 15);
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+          map
+        );
+      }
+
+  
+      if (marker) {
+        marker.setLatLng([coordinates.lat, coordinates.lng]);
+      } else {
+        marker = L.marker([coordinates.lat, coordinates.lng]).addTo(map);
+      }
     })
     .catch((error) => {
       console.error(error);
